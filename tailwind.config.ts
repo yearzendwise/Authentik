a99bfe49830d98e7a74793cf26d3cbc8bd30ec2c
Dan <dans3367@gmail.com>
2025-07-27 09:00:48 -0600

    Remove all custom form editor components and form add page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ export default {
   content: [
     "./client/index.html", 
     "./client/src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/drag-form-master/**/*.{js,jsx,ts,tsx}"
+
   ],
   theme: {
     extend: {
